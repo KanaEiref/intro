b296d481ff38e4f6a85d922f11bff430da3b9564
halogenandtoast <halogenandtoast@gmail.com>
2013-05-13 16:00:49 -0400

    Create decks.

--- flashcards/app/controllers/decks_controller.rb
+++ flashcards/app/controllers/decks_controller.rb
@@ -1,9 +1,19 @@
 class DecksController < ApplicationController
   def index
-
   end
 
   def new
+    @deck = Deck.new
+  end
 
+  def create
+    Deck.create deck_params
+    redirect_to "/decks"
+  end
+
+  private
+
+  def deck_params
+    params.require(:deck).permit(:name)
   end
 end
