857bffc5eb1e5fb22030cf3d359b9a04f821783a
halogenandtoast <halogenandtoast@gmail.com>
2013-05-13 14:48:26 -0400

    Generate rails app.

---
+++ flashcards/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flashcards::Application.config.secret_key_base = '39d29cf191ca9089970eabdaa15c3e7955538812d1eb748300f684d21f889847658f6c6f623031ef372df83bcf1a7c9a469e0710803cf447a642913501dcca19'
